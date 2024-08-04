420c7da6bc16b9a2e2e5cefeecf09892e4054d11
Kamal Singh <kamalsingh200238@gmail.com>
2024-08-04 15:48:23 +0100

    guest middleware

---
+++ app/Http/Middleware/OnlyGuest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class OnlyGuest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+
+            if ($user->isCoordinator()) {
+                return redirect()->route('coordinator.dashboard');
+            } else {
+                return redirect()->route('student.dashboard');
+            }
+
+        }
+
+        return $next($request);
+    }
+}
