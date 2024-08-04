ac93879a1bd0003c70c8f595d303cc648fdca97e
Kamal Singh <kamalsingh200238@gmail.com>
2024-08-04 12:08:57 +0100

    seed the db with sample data
    - 4 users and 1 society

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Society;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        DB::transaction(function () {
+            User::factory()->create([
+                'email' => 'coordinator@example.com',
+                'password' => '1',
+                'role' => 'society coordinator',
+            ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            // Create one president
+            $president = User::factory()->create([
+                'email' => 'president@example.com',
+                'password' => '1',
+                'role' => 'society president',
+            ]);
+
+            // Create one student
+            User::factory()->create([
+                'email' => 'student@example.com',
+                'password' => '1',
+                'role' => 'student',
+            ]);
+
+            // Create one student
+            User::factory()->unverified()->create([
+                'email' => 'student2@example.com',
+                'password' => '1',
+                'role' => 'student',
+            ]);
+
+            Society::factory()->create([
+                'name' => 'Basketball Society',
+                'is_active'  => true,
+                'president_id' => $president->id,
+            ]);
+        });
     }
 }
