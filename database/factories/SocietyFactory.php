31be6b56da9e198cf5d5d439fd7814a79569aac7
Kamal Singh <kamalsingh200238@gmail.com>
2024-08-04 12:08:22 +0100

    society factory to insert societies

---
+++ database/factories/SocietyFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Society;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use NunoMaduro\Collision\Adapters\Phpunit\State;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Society>
+ */
+class SocietyFactory extends Factory
+{
+    protected $model = Society::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->company,
+            'is_active' => true,
+            'president_id' => null,
+        ];
+    }
+}
