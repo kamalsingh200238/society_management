ade80a9556d61ee19c866ade73b0a65355d44eba
Kamal Singh <kamalsingh200238@gmail.com>
2024-08-04 15:48:39 +0100

    add aliases for middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,13 +1,15 @@
 <?php
 
+use App\Http\Middleware\CheckRoles;
+use App\Http\Middleware\OnlyGuest;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -16,7 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->alias([
+            'guest' => OnlyGuest::class,
+            'role' => CheckRoles::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
